e4c99e93ff91763dca347047132ab6ba53a84c4d
Maxim Naumov <ultimate.deej@gmail.com>
2015-04-01 13:52:52 +0300

    Store bundle in a property
    This will also allow to use different bundles in the future

--- Pod/Classes/UDLocalization.m
+++ Pod/Classes/UDLocalization.m
@@ -2,17 +2,30 @@
 
 #pragma mark Localizing
 
-static NSString *GetLocalizedString(NSString *key) {
+static NSString *GetLocalizedString(NSString *key, NSBundle *bundle) {
     if (![key isKindOfClass:[NSString class]]) return key;
-    return [[NSBundle bundleForClass:[UDLocalization class]] localizedStringForKey:key value:nil table:@"UDLocalizable"];
+    return [bundle localizedStringForKey:key value:nil table:@"UDLocalizable"];
 }
 
 #pragma mark - Class
 
+@interface UDLocalization ()
+
+@property(nonatomic) NSBundle *bundle;
+
+@end
+
 @implementation UDLocalization
 
+- (NSBundle *)bundle {
+    if (_bundle == nil) {
+        _bundle = [NSBundle bundleForClass:[self class]];
+    }
+    return _bundle;
+}
+
 - (NSString *)localizedString:(NSString *)key {
-    return GetLocalizedString(key);
+    return GetLocalizedString(key, self.bundle);
 }
 
 @end
